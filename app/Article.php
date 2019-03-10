cb801ebfc46e6e917ca0627a3342e133365c0d14
Milad <milad_abdollahnia@yahoo.com>
2019-03-10 14:51:19 +0330

    An attribute named catText for Article model that provide comma separated categories. Comma separated categories inside the article card. Comma separated categories instead of static sample text used in article show page.

--- app/Article.php
+++ app/Article.php
@@ -5,6 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Hekmatinasser\Verta\Verta;
 
+/**
+ * Class Article
+ * @package App
+ * @property Category[] $categories
+ * @property string catText All categories separated by comma.
+ */
 class Article extends Model
 {
 
@@ -51,4 +57,27 @@ class Article extends Model
         return $this->hasMany(Action::class);
     }
 
+    /**
+     * Attribute: catText
+     * @return array|\Illuminate\Contracts\Translation\Translator|null|string
+     */
+    protected function getCatTextAttribute() {
+        $categoryText = "";
+        // If article belongs to one or more categories.
+        if (!$this->categories->isEmpty()) {
+            // Style of comma.
+            $comma = trans("sign.comma") . " ";
+            // Convert all categories to a comma separated text.
+            foreach ($this->categories as $cat) {
+                $categoryText .= $cat->name . $comma;
+            }
+            // Remove last extra comma.
+            $categoryText = mb_substr($categoryText, 0, mb_strlen($comma) * -1);
+        } else {
+            // Alternative text when there is no categories for this article.
+            $categoryText = trans("main.uncat");
+        }
+
+        return $categoryText;
+    }
 }
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -85,39 +85,7 @@ body{
                     <ul  class="row">
 
                         @foreach ($articles as $article)
-
-                            <li class="col-lg-3 col-md-4 col-sm-6 card_col">
-
-                                <div style="padding-top:0;" class = "card pane active_pane">
-
-                                
-                                    <div class="card_img img-responsive img-thumbnail ratio-4-3" style="background-image:url('/images/articles_header_image/{{$article->image}}')">
-                                
-                                    <a href="{{ route('article.show' , ['article' => $article->id ]) }}"></a>
-                                    <footer class="image_footer">
-                                        <div class="image_footer_like"> <div class="like_counter"> {{$article->like}}</div> &nbsp <i style= "font-size:16px; margin-top:4px;" class="fa fa-heart-o" aria-hidden="true"></i>   </div>        
-                                        <div class="image_footer_auth"><i class="fa fa-user-o" aria-hidden="true"></i> &nbsp <a href="#" class="a auth_name"> محمد روایی </a> </div>
-                                                
-                                    </footer>
-                                    </div>
-
-                                    <div class="card_container">
-
-                                        <div class="card_container_text_part">
-
-                                            <h4><b><a class="a post_title" href="{{ route('article.show' , ['article' => $article->id ]) }}">{{$article->title}}</a></b></h4> 
-
-                                            <p class="post_cover" style="margin-top:0px;">
-                                                {{ str_limit(preg_replace( "/\r|\n/", "", $article->cover_text ) , 100) }}
-                                            </p>
-
-                                        </div>
-                                    
-                                    </div>
-
-                                </div>
-                            </li>
-
+                            @include("articles.card", ["article" => $article])
                         @endforeach
 
                     </ul>
